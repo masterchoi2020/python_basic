f3cf7f643d779f918ea529aba9583e17653d3a6e
unknown <99106128@naver.com>
2021-02-02 15:50:56 +0900

    python 기초

---
+++ basic_1/15.escape.py
@@ -0,0 +1,19 @@
+# 탈출 문자
+
+# \n : 줄 바꿈
+print("백문이 불여일견\n백견이 부려일타")
+
+# \" \' : 문장 내에서 따옴표
+print("저는 \"나도코딩\" 입니다.")
+print("저는 \'나도코딩\' 입니다.")
+
+# \\ : 문장 내에서 \
+# print("C:\\Users\\KITT\\Desktop\\pythonworkspace")
+
+# \r : 커서를 맨 앞으로 이동
+# print("Red Apple\rPine")
+
+# \t : 탭
+print("Red\tApple")
+
+
